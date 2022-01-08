45577db65a45b6b0d7c751f480ddeca13dc5ab6f
Charlie <charlie_jb@hotmail.co.uk>
2022-01-08 19:02:34 +0000

    Added basic MySQL integration

--- back-end/routes/breakdown.js
+++ back-end/routes/breakdown.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql')
+const config = require("../db.config.js");
 
-router.get('/', function(req, res, next) {
-    res.send('Data for breakdown');
+router.get('/', function (req, res, next) {
+
+    var connection = mysql.createConnection({
+        host: config.HOST,
+        user: config.USER,
+        password: config.PASSWORD,
+        database: config.DB
+    })
+
+    connection.connect()
+
+    connection.query('SELECT * FROM stocks WHERE belongs_to = "b"', function (err, rows, fields) {
+        if (err) throw err
+
+        res.send(rows);
+    })
+
+    connection.end()
+
 });
 
 module.exports = router;
