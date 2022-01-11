03746b282507f0a30f903df5c46a1d0e83164a14
Charlie <charlie_jb@hotmail.co.uk>
2022-01-11 15:50:51 +0000

    Started python file

--- data/process.py
+++ data/process.py
@@ -13,6 +13,16 @@ df_basic = pd.read_csv('./basic.csv')
 df_other = pd.read_csv('./other.csv')
 df_dividend = pd.read_csv('./dividend.csv')
 
-print(df_basic.head)
-print(df_other.head)
-print(df_dividend.head)
+# Populate the stocks table
+
+
+# Populate the daily table
+
+
+# Populate the stocks-daily table
+
+
+# Export all data
+# df_stocks.to_csv('./stocks.csv', index=False)
+# df_daily.to_csv('./daily.csv', index=False)
+# df_daily-stocks.to_csv('./stocks-daily.csv', index=False)
