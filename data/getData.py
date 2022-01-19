24e53530a3bae641f4d102a60ab60373c8b4d113
Charlie <charlie_jb@hotmail.co.uk>
2022-01-19 15:50:49 +0000

    Cleaned and optimised code

--- data/getData.py
+++ data/getData.py
@@ -14,81 +14,62 @@ df_basic = pd.read_csv('./inputs/basic.csv')
 df_other = pd.read_csv('./inputs/other.csv')
 
 # Format date fields
-df_basic['Date'] = pd.to_datetime(df_basic['Date'])
-df_other['Date'] = pd.to_datetime(df_other['Date'])
+df_basic['date'] = pd.to_datetime(df_basic['date'])
+df_other['date'] = pd.to_datetime(df_other['date'])
 
 # Split buys and sells into separate dataframes
-df_buys = df_basic.loc[df_basic["Type"] == "BUY"]
-df_sells = df_basic.loc[df_basic["Type"] == "SELL"]
+df_buys = df_basic.loc[df_basic["type"] == "BUY"]
+df_sells = df_basic.loc[df_basic["type"] == "SELL"]
 
 # Get the earliest transaction date
-minDate = min(df_basic["Date"].min(), df_other["Date"].min())
+minDate = min(df_basic["date"].min(), df_other["date"].min())
+yesterday = pd.to_datetime("today") - timedelta(days=1)
 
 # A list of all the tickers
-tickers_all = df_basic['Name'].unique()
+tickers_all = df_basic['ticker'].unique()
 
 # A list of all tickers with their currency
-tickers_currency = df_buys[["Name", "Currency"]].drop_duplicates().reset_index(drop=True)
+tickers_currency = df_buys[["ticker", "currency"]].drop_duplicates().reset_index(drop=True)
 
 # A list of currently held tickers
-tickers_current = getDayPortfolio(df_buys, df_sells)["Name"].explode().unique() 
+tickers_current = getDayPortfolio(df_buys, df_sells)["ticker"].explode().unique() 
+
+# Fill the df with all days from first investment to yeasterday, and linearly interpolate missing values in the given columns
+def fillDays(df, columns, start = minDate, end = yesterday):
+    # Fill df with all days
+    dates = pd.date_range(start, end).to_series(name="date")
+    df = pd.merge(dates, df, on="date", how="left", sort=True)
+
+    # Interpolate missing values
+    df[columns] = df[columns].interpolate(limit_area = "inside")
+
+    return df.fillna(0)
 
 ########################
 #  Get Overall Market  #
 ########################
 
 # Get the data
-df_spy = web.DataReader("SPY", 'yahoo', minDate, pd.to_datetime("today")).reset_index()
-df_ftse = web.DataReader("VUKE.L", 'yahoo', minDate, pd.to_datetime("today")).reset_index()
+df_spy = web.DataReader("SPY", 'yahoo', minDate, yesterday).reset_index()
+df_ftse = web.DataReader("VUKE.L", 'yahoo', minDate, yesterday).reset_index()
 df_spy = df_spy[["Date", "Adj Close"]]
 df_ftse = df_ftse[["Date", "Adj Close"]]
 
 # Merge them together
 df_markets = pd.merge(df_spy, df_ftse, on="Date", how="outer", sort=True)
-df_markets = df_markets.rename(columns={"Adj Close_x": "spy", "Adj Close_y": "ftse"})
+df_markets = df_markets.rename(columns={"Date": "date", "Adj Close_x": "spy", "Adj Close_y": "ftse"})
 
-# Fill df with all days
-dates = pd.date_range(minDate, "today").to_series(name="Date")
-df_markets = pd.merge(dates, df_markets, on="Date", how="left", sort=True)
-
-# Interpolate missing values
-df_markets[["spy", "ftse"]] = df_markets[["spy", "ftse"]].interpolate(limit_area = "inside")
-df_markets = df_markets.fillna(0)
+df_markets = fillDays(df_markets, ["spy", "ftse"])
 
 ########################
 #  Get exchange rates  #
 ########################
 
 # Get any necessary exchange rates
-df_exchange_USDGBP = web.DataReader("GBPUSD=X", 'yahoo', minDate, pd.to_datetime("today"))
-df_exchange_USDGBP.drop(columns=["High", "Low", "Open", "Volume", "Adj Close"], inplace=True)
-df_exchange_USDGBP = df_exchange_USDGBP.reset_index()
-
-# Get the first and last date
-currentDate_fx = minDate
-lastDate_fx = pd.to_datetime("today")
-lastDate_fx = lastDate_fx.replace(hour=0, minute=0, second=0, microsecond=0)
-
-# Iterate through each day
-while currentDate_fx != lastDate_fx:
-    dayCounter_fx = 1
-
-    # If the current day doesn't have an exchnage rate entry
-    while len(df_exchange_USDGBP.loc[df_exchange_USDGBP["Date"] == currentDate_fx]) == 0:
-
-        # Get the previous days entry
-        prevDate_fx = currentDate_fx - timedelta(days=dayCounter_fx)
-        df_previousFx = df_exchange_USDGBP.loc[df_exchange_USDGBP["Date"] == prevDate_fx]
-        df_previousFx["Date"] = currentDate_fx
-
-        # Add this entry to the exchange rate df
-        df_exchange_USDGBP = df_exchange_USDGBP.append(df_previousFx, ignore_index=True)
-        dayCounter_fx = dayCounter_fx + 1
-
-    currentDate_fx = currentDate_fx + timedelta(days=1)
-
-# Rename the Close column to avoid confusion in the next cell...
-df_exchange_USDGBP = df_exchange_USDGBP.rename(columns={"Close": "Rate"})
+df_exchange_USDGBP = web.DataReader("GBPUSD=X", 'yahoo', minDate, yesterday).reset_index()
+df_exchange_USDGBP = df_exchange_USDGBP[["Date", "Close"]]
+df_exchange_USDGBP = df_exchange_USDGBP.rename(columns={"Date": "date", "Close": "close_currency"})
+df_exchange_USDGBP = fillDays(df_exchange_USDGBP, ["close_currency"])
 
 ######################
 #  Get closing data  #
@@ -100,49 +81,49 @@ for ticker in tickers_all:
     df_closing_ticker = []
 
     # Get the first transaction date for the ticker
-    date_first = pd.to_datetime("today")
-    date_first = df_buys.loc[df_buys["Name"] == ticker]["Date"].min()
+    date_first = df_buys.loc[df_buys["ticker"] == ticker]["date"].min()
 
     # Get the last transaction date for the ticker, or the current date if it's still being held
-    date_last = pd.to_datetime("today")
+    date_last = yesterday
     if ticker not in tickers_current:
-        date_last = df_sells.loc[df_sells["Name"] == ticker]["Date"].max()
+        date_last = df_sells.loc[df_sells["ticker"] == ticker]["date"].max()
 
     # Get the close prices for each day
-    df_closing_ticker = web.DataReader(ticker, 'yahoo', date_first, date_last)    
+    df_closing_ticker = web.DataReader(ticker, 'yahoo', date_first, date_last).reset_index()
 
-    # If there are gaps in the dates, this will be dealt with later. However, we need to make sure we have a value on or before
-    #  the 'first' date in order to do this. Keep trying the previous days until we get a value
-    while date_first != df_closing_ticker.index.min():
+    # Make sure we have a 'first' date
+    while date_first != df_closing_ticker["Date"].min():
         date_first = date_first - timedelta(days=1)
-        df_closing_ticker = web.DataReader(ticker, 'yahoo', date_first, date_last)    
+        df_closing_ticker = web.DataReader(ticker, 'yahoo', date_first, date_last).reset_index()
 
     # Format the df
-    df_closing_ticker.drop(columns=["High", "Low", "Open", "Volume", "Adj Close"], errors="ignore", inplace=True)
-    df_closing_ticker["Name"] = ticker
+    df_closing_ticker = df_closing_ticker.rename(columns={"Date": "date", "Close": "close_native"})
+    df_closing_ticker = df_closing_ticker[["date", "close_native"]]
+    df_closing_ticker = fillDays(df_closing_ticker, "close_native", date_first, date_last)   
+    df_closing_ticker["ticker"] = ticker
 
     # Get the currency for the current ticker
-    tickerCurrency = tickers_currency.loc[tickers_currency["Name"] == ticker, "Currency"].values[0]
+    tickerCurrency = tickers_currency.loc[tickers_currency["ticker"] == ticker, "currency"].values[0]
 
     # If it's USD, divide by the exchange rate for the relevant day
     if tickerCurrency == "USD":
-        df_closing_ticker = pd.merge(df_closing_ticker, df_exchange_USDGBP, how="left", on="Date")
-        df_closing_ticker["Close"] = df_closing_ticker["Close"]/df_closing_ticker["Rate"]
-        df_closing_ticker.drop(columns=["Rate"], inplace=True)
+        df_closing_ticker = pd.merge(df_closing_ticker, df_exchange_USDGBP, how="left", on="date")
+        df_closing_ticker["close_native"] = df_closing_ticker["close_native"]/df_closing_ticker["close_currency"]
+        df_closing_ticker.drop(columns=["close_currency"], inplace=True)
 
     # If it's GBp, divide by 100 to convert to £
     elif tickerCurrency == "GBP":
-        df_closing_ticker["Close"] = df_closing_ticker["Close"]/100
+        df_closing_ticker["close_native"] = df_closing_ticker["close_native"]/100
     else:
         print("ERROR: Unidentified Currency - ", ticker)
-    
-    df_closing_all.append(df_closing_ticker.reset_index())
+
+    df_closing_ticker = df_closing_ticker.rename(columns={"close_native": "close"})
+
+    df_closing_all.append(df_closing_ticker)
 
 # Concatenate all data
 df_closing_all = pd.concat(df_closing_all)
-df_closing_all.drop(columns=["index"], inplace=True)
 
 
 df_closing_all.to_csv('./closingPrices.csv', index=False)
-df_exchange_USDGBP.to_csv('./USDGBP_exchange.csv', index=False)
 df_markets.to_csv('./markets.csv', index=False)
--- data/utils.py
+++ data/utils.py
@@ -1,36 +1,37 @@
 import pandas as pd
+from datetime import timedelta
 
 # Get a valid pandas date
 def getDateFormat(day, month, year):
     return pd.to_datetime(f"{year}-{month}-{day}")
 
 # Get the shares held on a given day
-def getDayPortfolio(df_buys, df_sells, day = pd.to_datetime("today")):
+def getDayPortfolio(df_buys, df_sells, day = pd.to_datetime("today") - timedelta(days=1)):
 
     # Get the buys and sells for each day before the given date
-    df_buysInRange = df_buys.loc[df_buys["Date"] <= day, ["Date", "Name", "ShareCount"]]
-    df_sellsInRange = df_sells.loc[df_sells["Date"] <= day, ["Date", "Name", "ShareCount"]]
+    df_buysInRange = df_buys.loc[df_buys["date"] <= day, ["date", "ticker", "shareCount"]]
+    df_sellsInRange = df_sells.loc[df_sells["date"] <= day, ["date", "ticker", "shareCount"]]
 
     # Any missing sells, i.e. any stock we've bought but not sold any of, need to still be in the resulting 'sell' df
     df_emptySells = df_buysInRange.copy()
-    df_emptySells["ShareCount"] = 0
+    df_emptySells["shareCount"] = 0
     df_sellsInRange = pd.concat([df_sellsInRange, df_emptySells])
 
     # Aggregate to get total shares in and out
-    sharesIn = df_buysInRange.groupby('Name').agg({'ShareCount': 'sum'})['ShareCount'].reset_index()
-    sharesOut = df_sellsInRange.groupby('Name').agg({'ShareCount': 'sum'})['ShareCount'].reset_index()
+    sharesIn = df_buysInRange.groupby('ticker').agg({'shareCount': 'sum'})['shareCount'].reset_index()
+    sharesOut = df_sellsInRange.groupby('ticker').agg({'shareCount': 'sum'})['shareCount'].reset_index()
 
     # Format the aggregations
-    sharesIn.rename(columns={"ShareCount": "SharesIn"}, inplace=True)
-    sharesOut.rename(columns={"ShareCount": "SharesOut"}, inplace=True)
-    sharesOut.drop(columns="Name", inplace=True)
+    sharesIn.rename(columns={"shareCount": "sharesIn"}, inplace=True)
+    sharesOut.rename(columns={"shareCount": "sharesOut"}, inplace=True)
+    sharesOut.drop(columns="ticker", inplace=True)
 
     # Calculate net shares
     sharesTotal = pd.concat([sharesIn, sharesOut], axis=1)
-    sharesTotal["ShareCount"] = (sharesTotal["SharesIn"] - sharesTotal["SharesOut"]).round(5)
+    sharesTotal["shareCount"] = (sharesTotal["sharesIn"] - sharesTotal["sharesOut"]).round(5)
 
     # Format the portfolio
-    portfolio = sharesTotal.loc[sharesTotal["ShareCount"] != 0]
-    portfolio = portfolio.drop(columns=["SharesIn", "SharesOut"])
+    portfolio = sharesTotal.loc[sharesTotal["shareCount"] != 0]
+    portfolio = portfolio.drop(columns=["sharesIn", "sharesOut"])
 
     return portfolio
