3d73dc2232af9a47e922a58152d41590fd5f5544
Charlie <charlie_jb@hotmail.co.uk>
2022-01-23 00:29:38 +0000

    Started fixinf splits issue

--- data/utils.py
+++ data/utils.py
@@ -1,9 +1,40 @@
 import pandas as pd
+import yfinance as yf
+
 from datetime import timedelta
 
 # Get a valid pandas date
 def getDateFormat(day, month, year):
     return pd.to_datetime(f"{year}-{month}-{day}")
+
+# Get the number of shares held on a given day, taking splits into account
+def getShares(df, ticker, date):
+
+    # Get splits
+    splits = yf.Ticker(ticker).splits
+    df_split = pd.DataFrame({'date':splits.index, 'split':splits.values})
+
+    # Merge with raw sharecounts
+    df_combined =  pd.merge(df.loc[df["ticker"] == ticker, ["date", "shareCount"]], df_split, on="date", how="outer")
+
+    # Limit to relevant 
+    minDate_ticker = df.loc[df["ticker"] == ticker, "date"].min()
+    df_combined.drop(df_combined.loc[df_combined["date"] < minDate_ticker].index, inplace=True)
+
+    # Format the split column
+    df_combined.sort_values(by="date", ascending=False, inplace=True)
+    df_combined["split"] = df_combined["split"].fillna(0).cumsum()
+    df_combined.loc[df_combined["split"] == 0, "split"] = 1
+    df_combined.sort_values(by="date", inplace=True)
+
+    # Drop the split rows i.e. rows that aren't a buy/sell
+    df_combined.dropna(inplace=True)
+
+    # Recalculate sharecount
+    df_combined["shareCount"] = df_combined["shareCount"]*df_combined["split"]
+
+    return df_combined["shareCount"].sum()
+
 
 # Get the shares held on a given day
 def getDayPortfolio(df_buys, df_sells, day = pd.to_datetime("today") - timedelta(days=1)):
