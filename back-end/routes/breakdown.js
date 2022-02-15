4e627a9037fa9597fc4d31f2385324d378610cc4
Charlie <charlie_jb@hotmail.co.uk>
2022-02-15 21:37:52 +0000

    Formatted area graph data - needs optimising

--- back-end/routes/breakdown.js
+++ back-end/routes/breakdown.js
@@ -38,8 +38,33 @@ router.get('/', function (req, res, next) {
 
 });
 
-function getData_areaGraph(){
-    return daily_stocks
+function getData_areaGraph() {
+    Date.prototype.addDays = function (days) {
+        var date = new Date(this.valueOf());
+        date.setDate(date.getDate() + days);
+        return date;
+    }
+
+    let currentDate = new Date(daily_stocks[0].date)
+    let lastDate = new Date(daily_stocks[daily_stocks.length - 1].date)
+
+    let output = []
+
+    while (currentDate <= lastDate) {
+        let relevantDays = daily_stocks.filter(row => { return new Date(row.date).getDate() === currentDate.getDate() })
+
+        let day = { date: `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDay()}` }
+
+        if (relevantDays.length > 0) {
+            for (let s of relevantDays) {
+                day[s.ticker] = s.value
+            }
+        }
+        output.push(day)
+        currentDate.setDate(currentDate.getDate() + 1)
+    }
+
+    return output
 }
 
 module.exports = router;
